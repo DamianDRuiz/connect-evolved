2d8316893a7312af850260c6276e2d6b8bad411f
Damian <ruiz.damian17@gmail.com>
2023-10-23 18:06:53 -0400

    Tick lowest cell in column, switch player turn

--- libs/connected-evolved/feature-connect-game/src/lib/connected-evolved--feature-connect-game.spec.ts
+++ libs/connected-evolved/feature-connect-game/src/lib/connected-evolved--feature-connect-game.spec.ts
@@ -1,10 +1,33 @@
 import { ConnectGame } from './connected-evolved--feature-connect-game';
 
-import { NEW_BOARD } from './constants';
+import { COLUMN_COUNT, NEW_BOARD, ROW_COUNT } from './constants';
+import { Board } from './types';
+import { buildCells } from './util/buildCells';
 
-describe('ConnectGame', () => {
+describe('Initializing', () => {
   it('given nothing, should return a fresh board', () => {
     expect(ConnectGame()).toEqual(NEW_BOARD);
   });
 
+  it('given a board, with no action, should return the board', () => {
+    expect(ConnectGame(NEW_BOARD)).toEqual(NEW_BOARD)
+  })
 });
+
+describe('Cell clicking', () => {
+  it('given a board and action, should place a piece at the lowest empty cell', () => {
+    let subject: Board = {...NEW_BOARD}
+    const testedClickedCell = {x: 8, y: 1, ticked: false, owner: null}
+    const result: Board = {currentPlayer: 1, cells: buildCells(COLUMN_COUNT, ROW_COUNT)}
+
+    result.cells[55].ticked = true
+    result.cells[55].owner = 1
+    subject = ConnectGame(subject, testedClickedCell)
+    expect(subject.cells).toEqual(result.cells) // Clicked cell column is empty
+
+    result.cells[47].ticked = true
+    result.cells[47].owner = 2
+    subject = ConnectGame(subject, testedClickedCell)
+    expect(subject.cells).toEqual(result.cells) // Clicked cell column is not empty
+  })
+})
--- libs/connected-evolved/feature-connect-game/src/lib/connected-evolved--feature-connect-game.ts
+++ libs/connected-evolved/feature-connect-game/src/lib/connected-evolved--feature-connect-game.ts
@@ -1,10 +1,17 @@
-import { Action, Board } from "./types";
-import { buildCells } from "./util/buildCells";
+import { Action, Board, Cell } from "./types";
+import { getLowestEmptyCell } from "./util/getLowestEmptyCell";
 import { newBoard } from "./util/newBoard";
 
 export function ConnectGame(board?: Board, action?: Action ): Board  {
-  if(typeof board == "undefined")
-    board = newBoard(8, 7)
+  if(typeof board == "undefined") return newBoard(8, 7)
+  if(typeof action == "undefined") return board
+  if(action.ticked) return board
 
+  const updatedCell: Cell = board.cells.find((cell)=>cell == getLowestEmptyCell(action, board)) as Cell
+  updatedCell.ticked = true
+  updatedCell.owner = board.currentPlayer
+
+  board.currentPlayer = (board.currentPlayer == 1) ? 2 : 1;
+  
   return board;
 }
