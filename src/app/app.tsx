201416d63bb8abc5e1d1d2954c08ceb050145823
Damian <ruiz.damian17@gmail.com>
2023-10-23 18:45:27 -0400

    Main app uses Grid and Cell components

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,14 +1,21 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
+import { ConnectGame } from '../../libs/connected-evolved/feature-connect-game/src/lib/connected-evolved--feature-connect-game';
+import GameGrid from './components/GameGrid/GameGrid';
 
-import NxWelcome from './nx-welcome';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function App() {
+
+  const [board, setBoard] = useState(ConnectGame())
+  const handleCellClick = (e: MouseEvent) => {
+    const target = e.target as HTMLDivElement
+    const cellId: number = parseInt(target.id.split('-')[1])
+    const newBoard = ConnectGame(board, board.cells[cellId])
+    console.log(newBoard)
+    setBoard({...newBoard})
+  }
 
   return (
     <>
-      <NxWelcome title="connect-evolved" />
-
-      <div />
+      <GameGrid cells={board.cells} columnCount={8} handleCellClick={handleCellClick} />
     </>
   );
 }
